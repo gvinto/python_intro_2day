0a200cdde17fe16a2b59d64a48ac584e914db2bc
kwseow <seow_khee_wei@yahoo.com.sg>
2020-06-04 13:22:43 +0800

    Add files via upload

---
+++ sports.py
@@ -0,0 +1,10 @@
+fav_sport = input("Enter your favourite sports:")
+fav_sport = fav_sport.lower()
+
+if fav_sport == "soccer":
+    print("Your Favorite Sport is " + fav_sport)
+elif fav_sport == "basketball":
+    print("Your Favorite Sport is " + fav_sport)
+elif fav_sport=="badminton":
+    print("Your Favorite Sport is " + fav_sport)
+    
